ec85016f263491f01402885d03f747d6651c3d37
Sushant Jain <sushant@lessgo.ai>
2025-05-03 13:45:43 +0200

    sprint 2 promptpage design complete_v1

--- src/components/shared/Logo.tsx
+++ src/components/shared/Logo.tsx
@@ -2,6 +2,7 @@ import Image from "next/image"
 
 export default function Logo({ className = "", size = 40 }: { className?: string; size?: number }) {
     return (
+      <div className="flex flex-col items-center">
       <Image
         src="/logo.svg"
         alt="Lessgo.ai Logo"
@@ -10,5 +11,10 @@ export default function Logo({ className = "", size = 40 }: { className?: string
         className={className}
         priority
       />
+      <div className="mt-1 ml-4 text-center text-sm text-gray-700 font-medium tracking-wide">
+    Pretty doesn’t sell
+  </div>
+
+</div>
     )
   }
--- src/components/shared/PageIntro.tsx
+++ src/components/shared/PageIntro.tsx
@@ -8,10 +8,10 @@ export default function PageIntro() {
   
   return (
     <div className={"text-center"}>
- <h1 className="text-2xl md:text-heading2 font-bold text-brand-logo mb-5 md:mb-3 leading-snug text-center">
-  <span className="inline md:hidden">Build super high-converting landing page in minutes</span>
+ <h1 className="text-2xl md:text-heading2 font-bold text-brand-logo mt-6 mb-5 md:mb-3 leading-snug text-center">
+  <span className="inline md:hidden">Build high-converting landing page with persuasive copy</span>
   <span className="hidden md:inline">
-    Build super high-converting landing page <br /> with <span className="text-brand-accentPrimary">crystal&nbsp;clear&nbsp;copy</span> in minutes.
+  Build high-converting landing page <br /> with <span className="text-brand-accentPrimary">clear&nbsp;persuasive&nbsp;copy</span>
   </span>
 </h1>
 <p className="text-base md:text-xl text-brand-mutedText font-medium leading-relaxed text-center">
